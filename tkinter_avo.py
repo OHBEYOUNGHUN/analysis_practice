65e99136615e1f51423f2b3c98c2ff35ac01163b
OHBEYOUNGHUN <obh0422@gmail.com>
2020-12-26 00:14:38 +0900

    그래프 추가

--- tkinter_avo.py
+++ tkinter_avo.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter.simpledialog import *
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -7,6 +10,9 @@ df = pd.read_excel('C:/pyproject/pro1/forecast_yhat2.xlsx', engine="openpyxl")
 df.columns = ["num", "date", "forecast"]
 df_forecast = df[['date', 'forecast']]
 df_forecast = df_forecast.round(2)
+df_forecast['date'] = df_forecast['date'].astype(str).str.strip("forecast")
+df_forecast['date'] =pd.to_datetime(df_forecast['date'], format = '%Y-%m-%d %H:%M:%S',errors='raise')
+df_forecast['date'] = df['date'].dt.date
 df_date = df[['date']]
 df_date.head()
 df_yhat = df[['forecast']]
@@ -32,7 +38,7 @@ def print_data(self):
 # tkinter window 생성
 # listbox 생성
 window = Tk()
-window.geometry("300x380")
+window.geometry("1000x800")
 
 # 스크롤바
 scrollbar = Scrollbar(window)
@@ -57,5 +63,20 @@ date_w = Label(window, text=" ")
 date_w.pack()
 forecast_w = Label(window, text=" ")
 forecast_w.pack()
+
+#캔버스
+# canvas = Canvas(window, height = 300, width = 300)
+# canvas.pack()
+# #이미지
+# img = PhotoImage(file = 'C:/Users/82102/Pictures/Saved Pictures/avocado_analysis/forecast.png')
+# canvas.create_image(500, 250, image =img)
+
+figure2 = plt.Figure(figsize= (10,7), dpi = 100)
+ax2 = figure2.add_subplot(111)
+line2 = FigureCanvasTkAgg(figure2, window)
+line2.get_tk_widget().pack(side= tkinter.LEFT, fill =tkinter.BOTH)
+df2 = df_forecast[['date', 'forecast']].groupby('date').sum()
+df2.plot(kind = 'line', legend=True, ax = ax2, color ='g', fontsize=10)
+ax2.set_title('avocado_forecast')
 
 window.mainloop()
